aa5500a8b04f67f8374b19daf0a98e21dd440f21
koshidefi <85802895+koshidefi@users.noreply.github.com>
2021-06-16 05:35:24 +0700

    Initial commit

---
+++ src/components/Value/Value.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import styled from "styled-components";
+import  { useCountUp } from 'react-countup';
+import numeral from "numeral";
+import { useEffect } from "react";
+interface ValueProps {
+  value: string;
+  valueSize?: string;
+  valueColor?: string;
+  valuePosition?: string;
+  valueBold?: string;
+  prefix?: string;
+  suffix?: string;
+ 
+}
+
+const Value: React.FC<ValueProps> = ({ value, valueSize, valueColor, valuePosition, valueBold, prefix, suffix }) => {
+  const valueCountUp= useCountUp({
+    start: 0,
+    end: numeral(value).value() ? numeral(value).value(): 0,
+    formattingFn: (val) => val ? `${suffix} ${numeral(val).format("0.00a")} ${prefix}` : "--",
+    decimals: 2,
+    duration: 1.75
+  });
+  useEffect(() => {
+    valueCountUp.update(numeral(value).value());
+   },[value] );
+  return (
+    <StyledValue valueSize={valueSize} valueColor={valueColor} valuePosition={valuePosition} valueBold={valueBold}>
+      {valueCountUp.countUp}
+    </StyledValue>
+  );
+};
+
+interface StyledValueProps {
+  valueSize?: string;
+  valueColor?: string;
+  valuePosition?: string;
+  valueBold?: string;
+}
+
+const StyledValue = styled.div<StyledValueProps>`
+  color: ${(props) => (props.valueColor ? props.valueColor : props.theme.textColor)};
+  font-size: ${(props) => (props.valueSize ? props.valueSize : "24px")};
+  font-weight: ${(props) => (props.valueBold ? props.valueBold : "700")};
+  text-align: ${(props) => (props.valuePosition ? props.valuePosition : "left")};
+  line-height: ${(props) => (props.valueSize ? props.valueSize : "normal")};
+`;
+
+export default Value;
---
+++ src/views/Farm/Farm.tsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useMemo, useRef } from "react";
+
+import { Box, Button, Card, CardContent, Container, Separator, Spacer, useTheme } from "react-neu";
+
+import CountUp, { useCountUp } from 'react-countup';
+
+import numeral from "numeral";
+import Page from "components/Page";
+import PageHeader from "components/PageHeader";
+import Split from "components/Split";
+import useFarming from "hooks/useFarming";
+import HarvestCard from "./components/Harvest";
+import StakeCard from "./components/Stake";
+import PausedLPsNotice from "./components/PausedLPsNotice";
+import ResumedLPsNotice from "./components/ResumedLPsNotice";
+import HarvestLPsNoticeYAMYUSD from "./components/HarvestLPsNoticeYAMYUSD";
+import { useWallet } from "use-wallet";
+import FancyValue from "components/FancyValue";
+
+const Farm: React.FC = () => {
+  const { colors } = useTheme();
+  const { status } = useWallet();
+
+  const { tvl, apr, isRedeeming, onRedeemYAMETH } = useFarming();
+  const { countUp , start, update } = useCountUp({
+    start: 0,
+    end: tvl ? tvl: 0,
+    formattingFn: (val) => val ? `TVL $${numeral(val).format("000,000,000")}` : "Loading TVL...",
+    decimals: 0,
+    duration: 1.75
+  });
+  const aprCountUp= useCountUp({
+    start: 0,
+    end: apr ? apr: 0,
+    formattingFn: (val) => val ? `APR ${numeral(val).format("0.00a")}%` : "Loading APR...",
+    decimals: 2,
+    duration: 1.75
+  });
+  useEffect(() => {
+    console.log("tvl = ",tvl);
+    update(tvl);
+  }, [tvl]);
+  useEffect(() => {
+    console.log("apr = ",apr);
+    aprCountUp.update(apr);
+  }, [apr]);
+ 
+  
+  const RedeemButton = useMemo(() => {
+    if (status !== "connected") {
+      return <Button disabled text="Harvest &amp; Unstake YAM/ETH" variant="secondary" />;
+    }
+    if (!isRedeeming) {
+      return <Button onClick={onRedeemYAMETH} text="Harvest &amp; Unstake YAM/ETH" variant="secondary" />;
+    }
+    return <Button disabled text="Redeeming..." variant="secondary" />;
+  }, [isRedeeming, onRedeemYAMETH]);
+
+  return (
+    <Page>
+      <PageHeader icon="🌾🍠" subtitle="Stake YAM/ETH Sushiswap LP tokens and grow YAMs" title="Farm" />
+      <Container>
+        <HarvestLPsNoticeYAMYUSD />
+        <ResumedLPsNotice />
+        {/* <PausedLPsNotice /> */}
+        <Card>
+          <CardContent>
+            {/* <FancyValue
+              wrap
+              value={tvl ? `TVL $${numeral(tvl).format("000,000,000")}` : "Loading TVL..."}
+              valueSize="54px"
+              valueColor={colors.primary.main}
+              valueBold="800"
+              label={apr ? `APR ${numeral(apr).format("0.00a")}%` : "Loading APR..."}
+            /> */}
+            {/* <CountUp
+              start={0}
+              end={tvl ? tvl : 0}
+              formattingFn={(val) => val ? `TVL $${numeral(val).format("000,000,000")}` : "Loading TVL..."}
+              decimals={0}
+              duration={1.75}
+              className="farm-tvl"
+            /> */}
+            <div className="farm-tvl">{countUp}</div>
+            <div className = "farm-apr">{aprCountUp.countUp}</div>
+          </CardContent>
+        </Card>
+        <Spacer />
+        <Split>
+          <StakeCard />
+          <HarvestCard />
+        </Split>
+        <Spacer />
+        <Box row justifyContent="center">
+          {RedeemButton}
+        </Box>
+        <Spacer size="lg" />
+        <Separator />
+        <Spacer size="lg" />
+        <Split>
+          <Button full text="Addresses" to="/addresses" variant="secondary" />
+          <Button
+            full
+            text="Get YAM/ETH LP tokens"
+            href="https://exchange.sushiswapclassic.org/#/add/0x0aacfbec6a24756c20d41914f2caba817c0d8521/ETH"
+            variant="tertiary"
+          />
+        </Split>
+      </Container>
+    </Page>
+  );
+};
+
+export default Farm;
